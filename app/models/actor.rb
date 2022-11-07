69d9d0bb3ea042b0621a17707fdcfc1a66b6e36e
Joe Robbins <joerobbins@Joes-MacBook-Air.local>
2022-11-07 12:13:31 -0500

    commit tree fe243b7b8c49c3f7f1485d7b1bc46b9fa4766327

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters and has_many :shows, through: :characters 
+
+
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+
+    def list_roles
+        # returns an array with names, need a string
+        shows.map{|show| show.name} 
+        # + characters.map{|char| char.name}
+        
+    
+    end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    has_many :networks
+
+
+    def character
+        character
+    end
+
+    # def network
+    #     network
+    # end
 end
